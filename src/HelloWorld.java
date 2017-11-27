8e2bb14b5ea26781273fa5b7f037bb506d5ad80b
hansolji <hansolji@192.168.0.9>
2017-11-27 12:32:49 +0900

    hansol

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		args = new String[3];
+		args[0] = "조몰리에세상";
+		args[1] = "hi";
+		args[2] = "dddd";
+		
+		System.out.println("조몰리에세상");
+		System.out.println("안녕해요");
+		System.out.println(args[2]);
+	}
+
+}
